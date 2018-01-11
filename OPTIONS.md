bab13f13d7061242b4cdb0bcfe33f3836bd518f1
Vladimir Feskov <v@vladimirfeskov.com>
2018-01-11 11:50:15 +0100

    Improve docs

---
+++ OPTIONS.md
@@ -0,0 +1,18 @@
+# All options
+
+None of them are required, call `addBackToTop()` without params to get nice default looks
+
+```js
+addBackToTop({
+  id: 'back-to-top',
+  showWhenScrollTopIs: 1, // px
+  onClickScrollTo: 0, // px
+  scrollDuration: 100, // ms
+  innerHTML: '<strong>Go Up</strong>',
+  diameter: 56, // px
+  cornerOffset: 20, // px
+  backgroundColor: '#000',
+  textColor: '#fff',
+  zIndex: 1
+})
+```
--- scripts/update-docs.js
+++ scripts/update-docs.js
@@ -1,15 +1,15 @@
 const fs = require('fs')
 const { name, version } = require('../package.json')
 
-const docs = [ 'README.md', 'INSTALL.md', 'EXAMPLES.md' ]
+const docs = [ 'README.md', 'INSTALL.md', 'EXAMPLES.md', 'OPTIONS.md' ]
 docs.forEach(doc => {
   const content = fs.readFileSync(doc).toString()
 
   fs.writeFileSync(
     doc,
     content.replace(
-      new RegExp(`(https://unpkg.com/)[^@\)]+@[^/]*/`, 'g'),
-      `$1${name}@${version}/`
+      new RegExp(`${name}(/blob/v|/v|@)[^/]+`, 'g'),
+      match => match.replace(/[^/@v]+$/, version)
     )
   )
 })
